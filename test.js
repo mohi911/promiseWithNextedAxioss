4cb3da15c40fc40b1e58d9e61df54d320b59d6c9
mohi911 <31509848+mohi911@users.noreply.github.com>
2023-11-04 00:11:13 +0530

    Create test.js

---
+++ test.js
@@ -0,0 +1,49 @@
+const test = async () => {
+    console.log('A');
+    const messages =  ["1","2","3"]; 
+    const result = [];
+    const mappper = {"1":"ONE","2":"TWO","3":"THREE"}
+    let p;
+    try{
+        p = await Promise.all(messages.map(msg => {
+            console.log('B call -> '+ msg);
+            let waitTime = Math.ceil(Math.random() * 10000);
+            return new Promise((resolve, reject)=>{
+                if(waitTime<5000){
+                    setTimeout(() => resolve('response in :: '+ waitTime), waitTime)
+                }else{
+                    reject('error in :: '+ waitTime)
+                }
+            }).then(resp => {
+                const msgChild =  `B call ${msg} Processed :: ${resp}`;
+                return new Promise((resolve, reject) => {
+                    let childWaitTime = Math.ceil(Math.random() * 10000);
+                    if(childWaitTime<6000){
+                        setTimeout(() => resolve('response in :: '+ childWaitTime), childWaitTime)
+                    }else{
+                        reject('error in :: '+ childWaitTime)
+                    }
+                }).then(respChild => {
+                    result.push(mappper[msg])
+                    return `${msgChild} :: Processed child :: ${respChild}`
+                }).catch(err => {
+                    return `${msgChild} :: Processed child :: ${err}` 
+                })
+            }).catch(errChild => {
+                throw Error(`B call ${msg} Processed :: ${errChild}`)
+            })
+        }))
+
+        console.log('C');
+        console.log("Promise.All :: ", p)
+    }catch(err){
+        console.log('D');
+        console.log(err)
+    }
+
+    console.log('E')
+    console.log("Result :: "+ result)
+   
+}
+
+test()
